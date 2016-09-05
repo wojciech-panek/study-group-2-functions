38147ba518224147f6ed20718ef6e57dd293aa60
Filip Przydryga <fileepek@gmail.com>
2016-09-05 17:04:22 +0200

    Exercise 1: added forEach, map and filter functions.

--- app/src/exercise1/exercise1.js
+++ app/src/exercise1/exercise1.js
@@ -1,3 +1,24 @@
-export function forEach() {
-  console.log('forEach');
+import _ from 'ramda';
+
+export function forEach(array, callback) {
+  return _.addIndex(_.reduce)((prev, current, index) => {
+    callback(current, index);
+    return prev;
+  }, [], array);
+}
+
+export function map(array, callback) {
+  return _.addIndex(_.reduce)((prev, current, index) => {
+    prev.push(callback(current, index));
+    return prev;
+  }, [], array);
+}
+
+export function filter(array, callback) {
+  return _.addIndex(_.reduce)((prev, current, index) => {
+    if (callback(current, index)) {
+      prev.push(current);
+    }
+    return prev;
+  }, [], array);
 }
