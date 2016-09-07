3f968c836526a0166ff4e67158176fa9e3042a5b
Tymoteusz Bleja <tymoteusz.bleja@outlook.com>
2016-09-07 11:09:46 +0200

    implemented recursive forEach

--- app/src/exercise3/exercise3.js
+++ app/src/exercise3/exercise3.js
@@ -1,9 +1,16 @@
 import _ from 'ramda';
 
-export function forEach(collection, iteratee = _.identity) {
+const forEachRecursive = (collection, iteratee, index = 0) => {
+  if (index < collection.length && iteratee(_.nth(collection, index), index, collection) !== false) {
+    forEachRecursive(collection, iteratee, ++index);
+  }
+  return collection;
+};
 
+export function forEach(collection, iteratee = _.identity) {
+  forEachRecursive(collection, iteratee);
 }
 
 export function flatMapDeep(collection, iteratee = _.identity) {
-
+  
 }
