8fcdf7a2e125378410c636bc8ed939d41a522f21
Filip Przydryga <fileepek@gmail.com>
2016-09-29 10:39:36 +0200

    Finished first part of fourth exercise.

--- app/src/exercise4/exercise4.js
+++ app/src/exercise4/exercise4.js
@@ -1,8 +1,23 @@
 import _ from 'lodash';
-import data from './data.json';
+import data1 from './data1.json';
+import data2 from './data2.json';
+
+function averageLengthDisk(album) {
+  return _.sumBy(album.tracks, track => track.length) / album.tracks.length;
+}
+
+export function avgLengthEvery() {
+  return _.map(data1['media'], item => averageLengthDisk(item));
+}
+
+export function avgLengthOverall() {
+  return _.reduce(avgLengthEvery(), (acc, item, index, collection) => {
+    return acc + item / collection.length;
+  }, 0);
+}
 
 export function getAlbums() {
-  return data['release-groups']
+  return data2['release-groups']
     .filter(a => !_.some(a['secondary-types'], t => t === 'Compilation'))
     .sort((a1, a2) => new Date(a1['first-release-date']) - new Date(a2['first-release-date']))
     .map(a => _.pick(a, ['title', 'first-release-date']));
